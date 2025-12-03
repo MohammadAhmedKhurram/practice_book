03eb1d3405cc8ac1df4b7fb8ced3c8d4bba8e726
MohammadAhmedKhurram <162457856+MohammadAhmedKhurram@users.noreply.github.com>
2025-12-03 16:41:36 +0500

    -

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://MohammadAhmedKhurram.example.com',
+  url: 'https://mohammadahmedkhurram.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/book-project',
+  baseUrl: '/practice_book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
